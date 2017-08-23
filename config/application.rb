a908f0db6980f527998bafb0bde648b0b3fe0401
Matt Rayner <mattrayner1@gmail.com>
2017-08-23 15:11:40 +0100

    Add force_ssl when needed

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module PackageWithMe
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.force_ssl = true if ENV['FORCE_SSL']
   end
 end
