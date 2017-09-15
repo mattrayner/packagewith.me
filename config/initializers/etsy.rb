41f0e4973daa0b1708287f89404dd071c218272e
Matt Rayner <mattrayner1@gmail.com>
2017-09-15 23:07:02 +0100

    REGIS INTERNATIONA

--- config/initializers/etsy.rb
+++ config/initializers/etsy.rb
@@ -4,3 +4,16 @@ Etsy.api_key = ENV['ETSY_API_KEY']
 Etsy.api_secret = ENV['ETSY_API_SECRET']
 Etsy.silent_errors = false
 Etsy.permission_scopes = %w(transactions_r shops_rw)
+
+unless Rails.env.test?
+  countries_hash = {}
+
+  countries = Etsy::Country.find_all
+  countries.each do |country|
+    countries_hash[country.id] = country.name
+  end
+else
+  {}
+end
+
+ETSY_COUNTRIES = countries_hash
